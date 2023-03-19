6b9fecddda8aeeb3d7d25cdca44f7c9c02da25e8
cesarlopezt <cesarlopez2808@gmail.com>
2023-03-19 18:20:05 -0500

    Initial Commit

---
+++ MultiplicationForKids/MultiplicationForKidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationForKidsApp.swift
+//  MultiplicationForKids
+//
+//  Created by Cesar Lopez on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationForKidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
